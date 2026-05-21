1421fc7060e7c7edc60fa29dd681dc7957aed818
MohammedRayan-2015 <mahaakhatoon08@gmail.com>
2026-05-21 18:24:43 +0400

    Add files via upload

---
+++ Python Operators - I/countthenotes.py
@@ -0,0 +1,27 @@
+# Write a program to calculate the number of notes in the given amount?
+
+amount = int(input("Enter the amount : "))
+
+five_hundred = amount // 500
+
+two_hundered = amount // 200
+
+hundred = amount // 100
+
+fifty = amount // 50
+
+twenty = amount // 20
+
+tens = amount // 10
+
+print("No of 500 notes : ", five_hundred)
+
+print("No of 200 notes : ", two_hundered)
+
+print("No of 100 notes : ", hundred)
+
+print("No of 50 notes : ", fifty)
+
+print("No of 20 notes : ", twenty)
+
+print("No of 10 notes : ", tens)
---
+++ Python Operators - I/percentage.py
@@ -0,0 +1,15 @@
+# Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. Find the percentage he got?
+
+math = 40
+
+science = 70
+
+hindi = 50
+
+english = 60
+
+total = math + science + hindi + english
+
+percentage = (total / 400) * 100
+
+print(percentage)
